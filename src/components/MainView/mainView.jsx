b481a9f187a1c4493814c7f3b2b53b8c13100af5
Nazareno <nazarenorosales_@hotmail.com>
2024-06-01 15:31:12 -0300

    change on main styles and logic: Language, Degrees and query string

--- src/components/MainView/mainView.jsx
+++ src/components/MainView/mainView.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import styles from "./mainView.module.css";
 export default function MainView() {
+  const ES_MODE = "ES";
+  const EN_MODE = "EN";
+  const CELCIUS_MODE = "ºC";
+  const FARENHEINT_MODE = "ºF";
+
   const [location, setLocation] = useState({});
   const [weatherData, setWeatherData] = useState({});
-  const [unity, setUnity] = useState("Celsius");
+  const [unity, setUnity] = useState(CELCIUS_MODE);
+  const [lang, setLang] = useState(ES_MODE);
   const BASE_URL = import.meta.env.VITE_API_URL;
   const API_KEY = import.meta.env.VITE_API_KEY;
 
@@ -14,26 +20,23 @@ export default function MainView() {
   useEffect(() => {
     if (location.lon && location.lat) {
       fetch(
-        `${BASE_URL}?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}`
+        `${BASE_URL}?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}&lang=${lang}&units=metric`
       )
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
           const filteredData = {
             location: data?.name,
-            celsiusTemp: (data?.main.feels_like - 273.15).toFixed(2),
-            farentheinTemp: (
-              ((data?.main?.feels_like - 273.15) * 9) / 5 +
-              32
-            ).toFixed(2),
+            celsiusTemp: data?.main?.feels_like.toFixed(2),
+            farentheinTemp: ((data?.main?.feels_like * 9) / 5 + 32).toFixed(2),
             humidity: data?.main?.humidity,
-            weather: data?.weather[0],
+            weather: data?.weather[0].description,
             windSpeed: data?.wind?.speed,
           };
           setWeatherData(filteredData);
         });
     }
-  }, [location]);
+  }, [location, lang]);
 
   const getLocation = () => {
     if (navigator.geolocation) {
@@ -46,22 +49,30 @@ export default function MainView() {
     }
   };
   const handleUnityChange = () => {
-    setUnity(unity === "Celsius" ? "Farenheit" : "Celsius");
+    setUnity(unity === CELCIUS_MODE ? FARENHEINT_MODE : CELCIUS_MODE);
+  };
+  const handleLangChange = () => {
+    setLang(lang === EN_MODE ? ES_MODE : EN_MODE);
   };
   return (
     <>
       <section className={styles.mainView}>
-        <h1>El tiempo en {weatherData?.location}</h1>
-        <button onClick={handleUnityChange}> {unity} </button>
+        <h1>{weatherData?.location}</h1>
+        <div className={styles.lang} onClick={handleLangChange}>
+          {lang}
+        </div>
         <p>
-          Temperatura:{" "}
-          {unity === "Celsius"
-            ? weatherData?.celsiusTemp + " ºC"
-            : weatherData?.farentheinTemp + " ºF"}
+          {unity === CELCIUS_MODE
+            ? weatherData?.celsiusTemp + CELCIUS_MODE
+            : weatherData?.farentheinTemp + FARENHEINT_MODE}
         </p>
+        <button onClick={handleUnityChange}> {unity} </button>
         <p>Humedad: {weatherData?.humidity}%</p>
-        <h2>Clima</h2>
-        <p>{weatherData?.weather?.main}</p>
+        <p>
+          {weatherData?.weather?.charAt(0).toUpperCase() +
+            weatherData?.weather?.slice(1).toLowerCase() +
+            "."}
+        </p>
         <p>Viento: {weatherData?.windSpeed}</p>
       </section>
     </>
