273eca6337c7661cde8c2da1c6bf1763a55967a3
Nazareno <nazarenorosales_@hotmail.com>
2024-06-02 17:15:39 -0300

    fixin nexdaycards import

--- src/components/NextDaysCard/NextDaysCard.jsx
+++ src/components/NextDaysCard/NextDaysCard.jsx
@@ -2,7 +2,7 @@ import styles from "./nextDaysCard.module.css";
 export default function NextDaysCard({ temp, description }) {
   return (
     <div className={styles.cardDetail}>
-      <h3>{temp + "ºK"}</h3>
+      <h3>{temp +"ºK"}</h3>
       <p>{description}</p>
     </div>
   );
